b766985f341e4313caedfb244039efe2c6cb0ce9
Simos Pavlakis <paulakis.simos@gmail.com>
2021-08-04 16:44:31 +0300

    chore: replace GnosisTheme with emotion's Theme in Button styles

--- src/atoms/Button/styles.ts
+++ src/atoms/Button/styles.ts
@@ -1,8 +1,7 @@
-import { css, SerializedStyles } from "@emotion/react";
-import { GnosisTheme } from "../../theme/default/defaultTheme";
+import { css, SerializedStyles, Theme } from "@emotion/react";
 
 export const btnContainer = (
-  theme: GnosisTheme,
+  theme: Theme,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _props: { color: string; size: string },
 ): SerializedStyles => css`

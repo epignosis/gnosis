9ec9ebaf6aeec39ccc9ca1166b9c3e4b092328cb
John Tsevdos <tsevdosjohn@gmail.com>
2021-08-12 14:54:28 +0300

    minor test fix

--- src/atoms/Typography/Text.test.tsx
+++ src/atoms/Typography/Text.test.tsx
@@ -5,7 +5,7 @@ import { render, screen } from "@test-utils/render";
 
 describe("<Text>", () => {
   it("renders correctly", () => {
-    const text = faker.lorem.text();
+    const text = faker.lorem.sentence(150);
     render(<Text fontSize="md">{text}</Text>);
     const textEl = screen.getByText(text);
 

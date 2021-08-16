a8f48dbcb82d1afba77e9a1f7a2c5ada25d28b15
maryxan <maryxan96@hotmail.com>
2021-08-16 15:17:52 +0300

    fix: avatar test

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Story } from "@storybook/react/types-6-0";
 import Avatar from "./Avatar";
-// import { CertificateSVG } from "@icons/core";
+import { CertificateSVG } from "@icons/core";
 
 export default {
   component: Avatar,
@@ -23,10 +23,10 @@ export const AvatarImage: Story = (args) => (
   <Avatar src="https://plus.talentlms.com/assets/users/user2.png" alt="John Tsevdos" {...args} />
 );
 
-// export const IconAvatar: Story = (args) => (
-//   <Avatar {...args}>
-//     <CertificateSVG />
-//   </Avatar>
-// );
+export const IconAvatar: Story = (args) => (
+  <Avatar {...args}>
+    <CertificateSVG />
+  </Avatar>
+);
 
 export const StringAvatar: Story = (args) => <Avatar {...args}>JT</Avatar>;
--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import faker from "faker";
 import Avatar from "./Avatar";
 import { render, screen } from "@test-utils/render";
-// import { CertificateSVG } from "@icons/core";
+import { CertificateSVG } from "@icons/core";
 
 describe("<Avatar />", () => {
   it("renders with image", () => {
@@ -19,27 +20,35 @@ describe("<Avatar />", () => {
   });
 
   it("renders a string", () => {
-    render(<Avatar>SP</Avatar>);
+    const useInitials = faker.name.firstName()[0];
 
-    const avatarChildren = screen.getByText("SP");
+    render(<Avatar>{useInitials}</Avatar>);
+
+    const avatarChildren = screen.getByText(useInitials);
     const img = screen.queryByRole("img");
 
-    expect(avatarChildren).toHaveTextContent("SP");
+    expect(avatarChildren).toHaveTextContent(useInitials);
     expect(img).toBe(null);
   });
 
-  // it("renders an icon", () => {
-  //   render(
-  //     <Avatar>
-  //       <CertificateSVG data-testid="icon" />
-  //     </Avatar>,
-  //   );
+  it("renders an icon", () => {
+    render(
+      <Avatar>
+        <CertificateSVG data-testid="icon" />
+      </Avatar>,
+    );
 
-  //   const avatarChildren = screen.getByTestId(/avatar-children-container/i);
-  //   const icon = screen.getByTestId(/icon/i);
-  //   const img = screen.queryByRole("img");
+    const avatarChildren = screen.getByTestId(/avatar-children-container/i);
+    const icon = screen.getByTestId(/icon/i);
+    const img = screen.queryByRole("img");
 
-  //   expect(avatarChildren).toContainElement(icon);
-  //   expect(img).toBe(null);
-  // });
+    expect(avatarChildren).toContainElement(icon);
+    expect(img).toBe(null);
+  });
+
+  it("matches snapshot", () => {
+    const { container } = render(<Avatar>SP</Avatar>);
+
+    expect(container).toMatchSnapshot();
+  });
 });

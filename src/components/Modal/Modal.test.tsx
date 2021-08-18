3680222d9c6b004fed329894e8dcc10f2705318c
maryxan <maryxan96@hotmail.com>
2021-08-18 13:48:12 +0300

    test: fix Modal test

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -22,48 +22,60 @@ describe("<Modal>", () => {
     expect(body).toHaveTextContent("Test body");
     expect(footer).toHaveTextContent("Test footer");
   });
-});
 
-it("renders correctly without Footer", () => {
-  render(
-    <Modal isOpen onClose={mockedOnClose}>
-      <Modal.Header>Test header</Modal.Header>
-      <Modal.Body>Test body</Modal.Body>
-    </Modal>,
-  );
-  const header = screen.getByText("Test header");
-  const body = screen.getByText("Test body");
-  const footer = screen.queryByTestId("modal-footer");
+  it("renders correctly without Footer", () => {
+    render(
+      <Modal isOpen onClose={mockedOnClose}>
+        <Modal.Header>Test header</Modal.Header>
+        <Modal.Body>Test body</Modal.Body>
+      </Modal>,
+    );
+    const header = screen.getByText("Test header");
+    const body = screen.getByText("Test body");
+    const footer = screen.queryByTestId("modal-footer");
 
-  expect(header).toHaveTextContent("Test header");
-  expect(body).toHaveTextContent("Test body");
-  expect(footer).not.toBeInTheDocument();
-});
+    expect(header).toHaveTextContent("Test header");
+    expect(body).toHaveTextContent("Test body");
+    expect(footer).not.toBeInTheDocument();
+  });
 
-it("renders correctly without Header and Footer", () => {
-  render(
-    <Modal isOpen onClose={mockedOnClose}>
-      <Modal.Body>Test body</Modal.Body>
-    </Modal>,
-  );
-  const body = screen.getByText("Test body");
-  const header = screen.queryByTestId("modal-header");
-  const footer = screen.queryByTestId("modal-footer");
+  it("renders correctly without Header and Footer", () => {
+    render(
+      <Modal isOpen onClose={mockedOnClose}>
+        <Modal.Body>Test body</Modal.Body>
+      </Modal>,
+    );
+    const body = screen.getByText("Test body");
+    const header = screen.queryByTestId("modal-header");
+    const footer = screen.queryByTestId("modal-footer");
 
-  expect(body).toHaveTextContent("Test body");
-  expect(header).not.toBeInTheDocument();
-  expect(footer).not.toBeInTheDocument();
-});
+    expect(body).toHaveTextContent("Test body");
+    expect(header).not.toBeInTheDocument();
+    expect(footer).not.toBeInTheDocument();
+  });
 
-it("Header renders correctly with close btn", () => {
-  render(
-    <Modal isOpen onClose={mockedOnClose}>
-      <Modal.Header>Test header</Modal.Header>
-    </Modal>,
-  );
-  const closeBtn = screen.getByRole("button");
+  it("Header renders correctly with close btn", () => {
+    render(
+      <Modal isOpen onClose={mockedOnClose}>
+        <Modal.Header>Test header</Modal.Header>
+      </Modal>,
+    );
+    const closeBtn = screen.getByRole("button");
 
-  userEvent.click(closeBtn);
+    userEvent.click(closeBtn);
 
-  expect(mockedOnClose).toHaveBeenCalledTimes(1);
+    expect(mockedOnClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("matches snapshot", () => {
+    const { container } = render(
+      <Modal isOpen onClose={mockedOnClose}>
+        <Modal.Header>Test header</Modal.Header>
+        <Modal.Body>Test body</Modal.Body>
+        <Modal.Footer>Test footer</Modal.Footer>
+      </Modal>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
 });

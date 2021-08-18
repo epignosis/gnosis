b5a325e022f52cd370e2072da7cc9275eab97f4e
Simos Pavlakis <paulakis.simos@gmail.com>
2021-08-18 13:42:25 +0300

    feat: Add RadioGroup form elements

---
+++ src/components/FormElements/RadioGroup/Radio.tsx
@@ -0,0 +1,33 @@
+import React, { FC, InputHTMLAttributes } from "react";
+import { SerializedStyles } from "@emotion/react";
+import { radioButtonContainer } from "./styles";
+import { ExtendableProps } from "types/common";
+import { InputSize } from "@components/FormElements/Input/Input";
+
+export type RadioOption = {
+  value: string;
+  label: string;
+  disabled?: boolean;
+};
+
+export type RadioProps = ExtendableProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  RadioOption & {
+    id: string;
+    size?: InputSize;
+    inline?: boolean;
+  }
+>;
+
+const Radio: FC<RadioProps> = (props) => {
+  const { id, label, size = "md", inline = false, ...rest } = props;
+
+  return (
+    <div css={(theme): SerializedStyles => radioButtonContainer(theme, { size, inline })}>
+      <input id={id} type="radio" {...rest} />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+};
+
+export default Radio;

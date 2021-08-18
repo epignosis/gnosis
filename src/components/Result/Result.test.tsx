01a9ed6f401d59d3e78c09b733fbc63e28cf1afe
maryxan <maryxan96@hotmail.com>
2021-08-18 17:21:19 +0300

    test: add faker and snapshots on Result test

--- src/components/Result/Result.test.tsx
+++ src/components/Result/Result.test.tsx
@@ -1,19 +1,24 @@
 import React from "react";
+import faker from "faker";
 import Result from "./Result";
 import { screen, render } from "@test-utils/render";
 import { InfoSVG } from "@icons/core";
 
 describe("<Result/>", () => {
+  const titleTxt = faker.lorem.sentence();
+  const infoTxt = faker.lorem.paragraph();
+  const footerTxt = faker.lorem.sentence();
+
   it("renders correctly", () => {
-    render(<Result title="Test title" />);
+    render(<Result title={titleTxt} />);
 
-    const title = screen.getByText("Test title");
+    const title = screen.getByText(titleTxt);
 
-    expect(title).toHaveTextContent("Test title");
+    expect(title).toHaveTextContent(titleTxt);
   });
 
   it("renders with icon", () => {
-    render(<Result title="Test title" icon={InfoSVG} />);
+    render(<Result title={titleTxt} icon={InfoSVG} />);
 
     const icon = screen.getByTestId(/result-icon/i);
 
@@ -21,39 +26,78 @@ describe("<Result/>", () => {
   });
 
   it("renders info paragraph", () => {
-    render(<Result title="Test title" info="Test info" />);
+    render(<Result title={titleTxt} info={infoTxt} />);
 
-    const info = screen.getByText(/test info/i);
+    const info = screen.getByText(infoTxt);
 
-    expect(info).toHaveTextContent("Test info");
+    expect(info).toHaveTextContent(infoTxt);
   });
 
   it("renders with footer", () => {
-    render(<Result title="Test title" footer={<button>test action</button>} />);
+    render(<Result title={titleTxt} footer={<button>{footerTxt}</button>} />);
 
-    const footerBtn = screen.getByText(/test action/i);
+    const footerBtn = screen.getByText(footerTxt);
 
-    expect(footerBtn).toHaveTextContent("test action");
+    expect(footerBtn).toHaveTextContent(footerTxt);
   });
 
   it("renders with all props provided", () => {
     render(
       <Result
         icon={InfoSVG}
-        title="Test title"
-        info="Test info"
-        footer={<button>test action</button>}
+        title={titleTxt}
+        info={infoTxt}
+        footer={<button>{footerTxt}</button>}
       />,
     );
 
     const icon = screen.getByTestId("result-icon");
-    const title = screen.getByText("Test title");
-    const info = screen.getByText("Test info");
-    const footerBtn = screen.getByText(/test action/i);
+    const title = screen.getByText(titleTxt);
+    const info = screen.getByText(infoTxt);
+    const footerBtn = screen.getByText(footerTxt);
 
     expect(icon).toBeInTheDocument();
-    expect(title).toHaveTextContent("Test title");
-    expect(info).toHaveTextContent("Test info");
-    expect(footerBtn).toHaveTextContent("test action");
+    expect(title).toHaveTextContent(titleTxt);
+    expect(info).toHaveTextContent(infoTxt);
+    expect(footerBtn).toHaveTextContent(footerTxt);
+  });
+
+  it("matches snapshot with title", () => {
+    const { container } = render(<Result title="Test title" />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("matches snapshot with icon", () => {
+    const { container } = render(<Result title="Test title" icon={InfoSVG} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("matches snapshot with info", () => {
+    const { container } = render(<Result title="Test title" info="Test info" />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("matches snapshot with footer", () => {
+    const { container } = render(
+      <Result title="Test title" footer={<button>Test footer</button>} />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("matches snapshot with all props provided", () => {
+    const { container } = render(
+      <Result
+        icon={InfoSVG}
+        title="Test title"
+        info="Test info"
+        footer={<button>Test footer</button>}
+      />,
+    );
+
+    expect(container).toMatchSnapshot();
   });
 });

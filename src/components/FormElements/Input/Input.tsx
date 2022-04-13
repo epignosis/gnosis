ae4cf532a3cd29b5d443ad11680d2f333a7f887f
maryxan <maryxan078@gmail.com>
2022-04-13 14:35:42 +0300

    chore: input icon size

--- src/components/FormElements/Input/Input.tsx
+++ src/components/FormElements/Input/Input.tsx
@@ -47,6 +47,7 @@ const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
     inline: hasLabel && inline,
     disabled: Boolean(rest?.disabled),
   });
+  const iconHeight = size === "sm" ? 28 : 32;
 
   return (
     <div
@@ -58,13 +59,13 @@ const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
       <div className="input-wrapper">
         {IconBefore && (
           <span className="prefix-icon" data-testid="input-icon-before">
-            <IconBefore height={32} />
+            <IconBefore height={iconHeight} />
           </span>
         )}
         <input ref={forwardedRef} id={id} {...rest} />
         {IconAfter && (
           <span className="suffix-icon" data-testid="input-icon-after">
-            <IconAfter height={32} />
+            <IconAfter height={iconHeight} />
           </span>
         )}
       </div>

e661e8736056269e6e71e6254cf8adf6d0a4ac39
Thrasos Kafasis <thrasoskafasis@Epignosis-M170.local>
2022-12-06 13:12:21 +0200

    fix(loader): size and margin

--- src/components/Loaders/Loader.tsx
+++ src/components/Loaders/Loader.tsx
@@ -10,16 +10,26 @@ export type LoaderProps = React.HTMLAttributes<HTMLDivElement> & {
   size?: "md" | "lg";
 };
 
+const loaderSize = {
+  md: "0.375rem",
+  lg: "0.75rem",
+};
+
+const loaderMargin = {
+  md: "0.0625rem",
+  lg: "0.625rem",
+};
+
 const Loader: FC<LoaderProps> = ({ fullScreen = false, size = "lg", ...rest }) => {
   const { loader } = useTheme();
   return (
     <div
-      css={(): SerializedStyles => container({ fullScreen, size })}
+      css={(): SerializedStyles => container({ fullScreen })}
       data-testid="loader"
       {...rest}
-      className={`loading-container${rest?.className ? ` ${rest?.className}` : ""}`}
+      className={rest?.className ?? ""}
     >
-      <PulseLoader color={loader.color} />
+      <PulseLoader color={loader.color} size={loaderSize[size]} margin={loaderMargin[size]} />
     </div>
   );
 };
--- src/components/Loaders/styles.ts
+++ src/components/Loaders/styles.ts
@@ -1,34 +1,9 @@
 import { css, SerializedStyles } from "@emotion/react";
-import { LoaderSize } from "./Loader";
 
-const loaderMargin = {
-  md: "0.0625rem",
-  lg: "0.625rem",
-};
-
-const loaderSize = {
-  md: "0.375rem",
-  lg: "0.75rem",
-};
-
-export const container = ({
-  fullScreen,
-  size,
-}: {
-  fullScreen: boolean;
-  size: LoaderSize;
-}): SerializedStyles => css`
+export const container = ({ fullScreen }: { fullScreen: boolean }): SerializedStyles => css`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: ${fullScreen ? "100vh" : "auto"};
-
-  &.loading-container {
-    span > span {
-      width: ${loaderSize[size]};
-      height: ${loaderSize[size]};
-      margin: ${loaderMargin[size]};
-    }
-  }
 `;

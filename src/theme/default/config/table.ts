0bfc9b9fbdf2f6f2929be2d5d943e3634ad2ff91
Manos Smyrnakis <msmyrnakis@csd.uoc.gr>
2023-04-25 16:39:24 +0300

    fix: removing hardcoded border color table

--- src/theme/default/config/table.ts
+++ src/theme/default/config/table.ts
@@ -4,4 +4,5 @@ export default {
   border: colors.secondary.base,
   rowBackgroundColor: colors.secondary.lighter,
   rowHoverColor: hexToRGBA(colors.primary.lightest.toString(), 0.4),
+  borderBottomColor: hexToRGBA(colors.secondary.lightest.toString(), 0.4),
 };

9bd02dc4ebed4025bb04ecac66a3db869ea6b315
Alexander Antoniades <aantoniadis@epignosishq.com>
2023-06-19 16:33:24 +0300

    style(pagination): add hover effect in pagination (rows per page) list (#163)
    * style(pagination): add hover effect in pagination (rows per page) list
    
    * test: update snapshot

--- src/theme/default/config/pagination.ts
+++ src/theme/default/config/pagination.ts
@@ -6,6 +6,7 @@ export default {
   background: colors.primary.lightest,
   boxShadowColor: colors.secondary.light,
   hoverBackground: Color(colors.primary.lightest).alpha(0.25).string(),
+  hoverPaginationList: colors.secondary.lighter,
   emptyState: colors.white,
   textColor: colors.black,
 };

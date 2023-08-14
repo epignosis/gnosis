e2ec0416f95250ad8029c5682c6f6d5275741c39
Stylianos Fotios Konstantelos <48370514+SteliosKon@users.noreply.github.com>
2023-08-14 13:47:06 +0300

    fix(dropdown types): export dropdown types (#193)

--- src/index.ts
+++ src/index.ts
@@ -47,3 +47,4 @@ export { default as DefaultTheme } from "./theme/default/defaultTheme";
 // Types
 export type { Props as TableProps } from "./components/Table/Table";
 export type { Column, Row } from "./components/Table/types";
+export type { DropdownProps, DropdownItem } from "./components/Dropdown/types";

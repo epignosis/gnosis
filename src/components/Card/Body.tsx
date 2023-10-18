1ad9f78ae2e52a6facc726321b5cb8b2918c2110
maryxan <maryxan078@gmail.com>
2023-10-18 14:05:44 +0300

    refactor(body.tsx): add className on Body.tsx on Card component

--- src/components/Card/Body.tsx
+++ src/components/Card/Body.tsx
@@ -3,6 +3,10 @@ import { cardBody } from "./styles";
 
 export type BodyProps = FC;
 
-const Body: BodyProps = ({ children }) => <section css={cardBody}>{children}</section>;
+const Body: BodyProps = ({ children }) => (
+  <section css={cardBody} className="card-body-container">
+    {children}
+  </section>
+);
 
 export default Body;

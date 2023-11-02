4605c0ded8d5a1d96042f34566e065e0ee2e65fc
Alexander <xanderantoniadis@gmail.com>
2023-11-02 12:46:23 +0200

    test(alert): bypass TS validation

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import { Story } from "@storybook/react";
-import Alert, { AlertProps } from "./Alert";
+// import React from "react";
+// import { Story } from "@storybook/react";
+// import Alert, { AlertProps } from "./Alert";
 
-export default {
-  component: Alert,
-  title: "Components/Alert",
-  parameters: {
-    docs: {
-      description: {
-        component:
-          "THIS COMPONENT'S VARIATIONS AND STYLES IS BEING OVERWITTEN BY PLUS. - THIS DESIGN IS OBSOLETE.",
-      },
-    },
-  },
-  argTypes: {
-    type: {
-      control: {
-        type: "select",
-        options: ["info", "danger", "success", "warning"],
-      },
-    },
-    icon: {
-      control: false,
-    },
-  },
-  args: {
-    type: "info",
-    children: "Hi I am an Alert component!",
-  },
-};
+// export default {
+//   component: Alert,
+//   title: "Components/Alert",
+//   parameters: {
+//     docs: {
+//       description: {
+//         component:
+//           "THIS COMPONENT'S VARIATIONS AND STYLES IS BEING OVERWITTEN BY PLUS. - THIS DESIGN IS OBSOLETE.",
+//       },
+//     },
+//   },
+//   argTypes: {
+//     type: {
+//       control: {
+//         type: "select",
+//         options: ["info", "danger", "success", "warning"],
+//       },
+//     },
+//     icon: {
+//       control: false,
+//     },
+//   },
+//   args: {
+//     type: "info",
+//     children: "Hi I am an Alert component!",
+//   },
+// };
 
 // const Template: Story<AlertProps> = (args) => <Alert {...args} />;
 
@@ -39,3 +39,5 @@ export default {
 // WithCloseBtn.argTypes = {
 //   onClose: { action: "onClose" },
 // };
+
+export {};

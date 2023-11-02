2128f1e7aae870e903a908b3d18855d2fb732f74
Alexander Antoniades <aantoniadis@epignosishq.com>
2023-11-02 13:13:16 +0200

    chore(alert): disable alert component from storybook but still export it (#254)
    * chore(alert): disable alert component from storybook but still export it
    
    * test(alert): bypass TS validation

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,33 +1,43 @@
-import React from "react";
-import { Story } from "@storybook/react";
-import Alert, { AlertProps } from "./Alert";
+// import React from "react";
+// import { Story } from "@storybook/react";
+// import Alert, { AlertProps } from "./Alert";
 
-export default {
-  component: Alert,
-  title: "Components/Alert",
-  argTypes: {
-    type: {
-      control: {
-        type: "select",
-        options: ["info", "danger", "success", "warning"],
-      },
-    },
-    icon: {
-      control: false,
-    },
-  },
-  args: {
-    type: "info",
-    children: "Hi I am an Alert component!",
-  },
-};
+// export default {
+//   component: Alert,
+//   title: "Components/Alert",
+//   parameters: {
+//     docs: {
+//       description: {
+//         component:
+//           "THIS COMPONENT'S VARIATIONS AND STYLES IS BEING OVERWITTEN BY PLUS. - THIS DESIGN IS OBSOLETE.",
+//       },
+//     },
+//   },
+//   argTypes: {
+//     type: {
+//       control: {
+//         type: "select",
+//         options: ["info", "danger", "success", "warning"],
+//       },
+//     },
+//     icon: {
+//       control: false,
+//     },
+//   },
+//   args: {
+//     type: "info",
+//     children: "Hi I am an Alert component!",
+//   },
+// };
 
-const Template: Story<AlertProps> = (args) => <Alert {...args} />;
+// const Template: Story<AlertProps> = (args) => <Alert {...args} />;
 
-export const Default = Template.bind({});
+// export const Default = Template.bind({});
 
-export const WithCloseBtn = Template.bind({});
+// export const WithCloseBtn = Template.bind({});
 
-WithCloseBtn.argTypes = {
-  onClose: { action: "onClose" },
-};
+// WithCloseBtn.argTypes = {
+//   onClose: { action: "onClose" },
+// };
+
+export {};

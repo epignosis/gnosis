e85a69ae78388b1ce484af7d7e23bcb94c529278
Thrasos Kafasis <thrasoskafasis@Epignosis-M170.local>
2023-11-23 10:03:30 +0200

    fix(input): allow arrow up and arrow down keys in number input

--- src/components/FormElements/Input/helpers.ts
+++ src/components/FormElements/Input/helpers.ts
@@ -2,13 +2,13 @@ export const preventNonNumericalInput = (
   e: React.KeyboardEvent<HTMLInputElement>,
   isNumberType: boolean,
 ): void => {
-  const allowedChars = ["Backspace", ".", ","];
+  const allowedChars = ["Backspace", ".", ",", "ArrowUp", "ArrowDown"];
   const numberPattern = /[0-9]/;
 
   const isCharacterNumber = numberPattern.test(e.key);
-  const isAllowrdChar = allowedChars.includes(e.key);
+  const isAllowedChar = allowedChars.includes(e.key);
 
-  if (!isCharacterNumber && !isAllowrdChar && isNumberType) {
+  if (!isCharacterNumber && !isAllowedChar && isNumberType) {
     e.preventDefault();
   }
 };

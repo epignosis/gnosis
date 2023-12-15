1eba5d42a6c46456a538ba44812d4c77fd8f4dc0
Thrasos Kafasis <thrasoskafasis@Epignosis-M170.local>
2023-12-15 16:36:00 +0200

    Revert "fix(radio, checkbox): fix styles and readonly property"
    This reverts commit 0d5e165a92a2158828296e9969c83aaeadcca5b1.

--- src/components/FormElements/RadioGroup/styles.ts
+++ src/components/FormElements/RadioGroup/styles.ts
@@ -19,11 +19,8 @@ export const radioButtonContainer = (
   };
 
   return css`
-    display: ${inline ? "inline-flex" : "flex"};
     padding-inline: 0 ${inline ? "1.5rem" : "0"};
     padding-block: 0.75rem;
-    position: relative;
-    align-items: center;
 
     input[type="radio"] {
       opacity: 0;
@@ -57,10 +54,10 @@ export const radioButtonContainer = (
       }
 
       + label {
-        position: relative;
-        display: flex;
-        padding-inline-start: 1.5rem;
         font-size: ${fontSizes[size]};
+        position: relative;
+        display: inline-block;
+        padding-inline-start: 1.5rem;
         cursor: ${readOnly ? "normal" : "pointer"};
 
         &::before {

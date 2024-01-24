3773e07a7860ac65954c9051a5071801ae2f7927
Alexander <xanderantoniadis@gmail.com>
2024-01-24 16:11:34 +0200

    refactor(helper function): refactor to avoid polynomial regular expression

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,17 @@
 export const convertToDataTestIdName = (input: string | undefined): string => {
   if (!input) return "";
 
-  let finalString = input.toLowerCase().replace(/\s+/g, "-").replace(/-+$/, "");
+  // Split the string into an array of words
+  const wordsArray = input.split(/\s+/);
+
+  // Convert each word to lowercase and join them back together with "-"
+  let finalString = wordsArray.map((word) => word.toLowerCase()).join("-");
+
+  // Remove trailing "-" characters
+  finalString = finalString.replace(/-+$/, "");
 
   // Remove other special characters
   finalString = finalString.replace(/[^a-z0-9-]/g, "");
+
   return finalString;
 };

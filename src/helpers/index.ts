474daf9634ece226824038793cba3d37a419e478
Alexander <xanderantoniadis@gmail.com>
2024-01-24 16:17:04 +0200

    refactor(helper function): refactor to avoid polynomial regular expression warning

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,17 +1,15 @@
 export const convertToDataTestIdName = (input: string | undefined): string => {
   if (!input) return "";
 
-  // Split the string into an array of words
-  const wordsArray = input.split(/\s+/);
+  const sanitizedString = input
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, "");
 
-  // Convert each word to lowercase and join them back together with "-"
-  let finalString = wordsArray.map((word) => word.toLowerCase()).join("-");
-
-  // Remove trailing "-" characters
-  finalString = finalString.replace(/-+$/, "");
-
-  // Remove other special characters
-  finalString = finalString.replace(/[^a-z0-9-]/g, "");
+  // Remove trailing hyphens without using regular expressions
+  const lastHyphenIndex = sanitizedString.lastIndexOf("-");
+  const finalString =
+    lastHyphenIndex !== -1 ? sanitizedString.slice(0, lastHyphenIndex + 1) : sanitizedString;
 
   return finalString;
 };

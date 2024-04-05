9e30aaf5e4edc8522fd8b48a9e53bf511f2e0611
Alexander Antoniades <aantoniadis@epignosishq.com>
2024-04-05 16:53:33 +0300

    refactor(icons): total refactor on the icons

--- src/icons/social/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as LinkedInColorSVG } from "./linkedin.svg";
-export { default as TwitterSVG } from "./twitter.svg";
-export { default as TwitterlogoSVG } from "./twitter-logo.svg";
-export { default as LinkedInSVG } from "./linkedin-icon.svg";
-export { default as FacebookLogoBlackSVG } from "./facebook-logo.svg";
-export { default as FacebookLogoSVG } from "./facebook.svg";
-export { default as GoogleSVG } from "./google.svg";

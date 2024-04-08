a9e39bb8dc16ee5d1137136ff2d859917bb73163
Alexander Antoniades <aantoniadis@epignosishq.com>
2024-04-08 10:54:13 +0300

    chore(icons): sync Icons pt.1 (#316)
    * chore(icons): sync Icons pt.1
    
    * chore(icons): categorise, add search, and fix naming
    
    * chore(icons): add copy functionality and search, also fix naming
    
    * chore(icons): make a reusable component for the lists. Rename some icons
    
    * chore(icons): chore
    
    * refactor(icons): total refactor on the icons
    
    * chore(icons): remove duplicate icons that are not used in the client
    
    * chore(icons): pR fix
    
    * chore(icons): remove hard-coded white in some SVGs

--- src/icons/Icons.stories.tsx
+++ src/icons/Icons.stories.tsx
@@ -1,45 +1,14 @@
 import React from "react";
-import { Text } from "../";
+import IconsList from "./IconsList/IconsList";
 import * as SvgIcons from ".";
 
 export default {
-  title: "Theme/Icons",
+  title: "Theme/Icons/General",
   parameters: {
     controls: { hideNoControlsWarning: true },
   },
 };
 
 export const Icons = (): JSX.Element => {
-  const icons = Object.values(SvgIcons);
-
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill,150px)",
-        columnGap: "2rem",
-        rowGap: "2rem",
-      }}
-    >
-      {icons.map((Icon, i) => (
-        <div key={i} style={{ textAlign: "center" }}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: 80,
-              width: 80,
-              boxShadow: "0px 0px 10px #00000026",
-              borderRadius: 5,
-              margin: "0 auto 1rem",
-            }}
-          >
-            <Icon height={32} color="black" />
-          </div>
-          <Text fontSize="2xs">{Icon.name}</Text>
-        </div>
-      ))}
-    </div>
-  );
+  return <IconsList svgIcons={SvgIcons} />;
 };

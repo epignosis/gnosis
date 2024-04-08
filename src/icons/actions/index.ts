11e53c5c3e671d323a20a4a2a19623d11d6cc6c0
Alexander Antoniades <aantoniadis@epignosishq.com>
2024-04-08 14:45:26 +0300

    chore(icons): replace underscores with dashes in svg filenames
    n

--- src/icons/actions/index.ts
+++ src/icons/actions/index.ts
@@ -1,4 +1,4 @@
 // Action Icons
-export { default as AddCourseIconSVG } from "./add_course_icon.svg";
+export { default as AddCourseIconSVG } from "./add-course-icon.svg";
 export { default as AddContentSVG } from "./add-content.svg";
-export { default as AdditionEventSVG } from "./addition_event.svg";
+export { default as AdditionEventSVG } from "./addition-event.svg";

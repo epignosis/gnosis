ddebde86099e89d06438a620d3f0cb083f7dc23b
maryxan <maryxan078@gmail.com>
2024-04-16 10:57:52 +0300

    chore(icons): add rating icons
    Add rating icons to feature folders

---
+++ src/icons/feature/rating/Icons.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import IconsList from "../../IconsList/IconsList";
+import * as SvgIcons from ".";
+
+export default {
+  title: "Theme/Icons/Feature/Rating",
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+};
+
+export const Icons = (): JSX.Element => {
+  return <IconsList svgIcons={SvgIcons} />;
+};

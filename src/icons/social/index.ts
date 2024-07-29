99129e8f26cec1999f2ebc8c4e20ddf4dd973eb8
Thrasyvoulos Kafasis <thrasyvoulos.kafasis@gmail.com>
2024-07-29 11:49:46 +0300

    chore(icons): linkedin logo icon (#372)
    Co-authored-by: Thrasos Kafasis <thrasoskafasis@Epignosis-M170.local>

--- src/icons/social/index.ts
+++ src/icons/social/index.ts
@@ -2,6 +2,7 @@
 export { default as GoogleSVG } from "./google.svg";
 export { default as FacebookSVG } from "./facebook.svg";
 export { default as LinkedinSVG } from "./linkedin.svg";
+export { default as TwitterSVG } from "./twitter.svg";
 export { default as FacebookLogoSVG } from "./facebook-logo.svg";
 export { default as TwitterLogoSVG } from "./twitter-logo.svg";
-export { default as TwitterSVG } from "./twitter.svg";
+export { default as LinkedinLogoSVG } from "./linkedin-logo.svg";

f2f0407c29c346e6d85a04698f2996ba71c7044f
George Nasiopoulos <gnasiopoulos@epignosishq.com>
2024-09-20 11:40:45 +0300

    feat(input): allows Input component to take a tooltip icon as a prop

--- src/components/FormElements/Input/Input.tsx
+++ src/components/FormElements/Input/Input.tsx
@@ -6,6 +6,7 @@ import React, {
   MouseEvent,
   useEffect,
   isValidElement,
+  ReactElement,
 } from "react";
 import classNames from "classnames";
 import { SerializedStyles } from "@emotion/react";
@@ -32,6 +33,7 @@ export type InputProps = ExtendableProps<
     containerAttrs?: React.HTMLAttributes<HTMLDivElement>;
     css?: SerializedStyles;
     tooltipContent?: string | JSX.Element;
+    toolTipIcon?: ReactElement;
     showVerticalLine?: boolean;
     isClearable?: boolean;
     autoFocus?: boolean;
@@ -51,6 +53,7 @@ const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
     id,
     containerAttrs,
     tooltipContent = "",
+    toolTipIcon,
     value,
     isClearable = false,
     showVerticalLine = true,
@@ -124,7 +127,7 @@ const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
           {shouldRenderTooltip && (
             <div data-testid={`${id}-tooltip`}>
               <Tooltip content={tooltipContent}>
-                <InfoCircledSVG height={20} />
+                {toolTipIcon || <InfoCircledSVG height={20} />}
               </Tooltip>
             </div>
           )}

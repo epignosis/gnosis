24865423155a474764991aeaf20749842d267586
Isidoros Lemonidis <ilemonidis@epignosishq.com>
2024-10-04 15:16:46 +0300

    fix: remove media player from storybook

--- src/components/MediaPlayer/VideoPlayer/types.ts
+++
@@ -1,6 +0,0 @@
-import { ReactPlayerProps } from "react-player";
-
-export type PlayerProps = Omit<ReactPlayerProps, "url"> & {
-  src: ReactPlayerProps["url"];
-  containerAttrs?: React.HTMLAttributes<HTMLDivElement>;
-};
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ export { default as Alert } from "./components/Alert/Alert";
 export { default as ProgressBar } from "./components/ProgressBar/ProgressBar";
 export { default as Tooltip } from "./components/Tooltip/Tooltip";
 export { default as Modal } from "./components/Modal/Modal";
-export { default as MediaPlayer } from "./components/MediaPlayer/MediaPlayer";
 export { default as Pagination } from "./components/Pagination/Pagination";
 export { default as Tabs } from "./components/Tabs/Tabs";
 export { default as Result } from "./components/Result/Result";

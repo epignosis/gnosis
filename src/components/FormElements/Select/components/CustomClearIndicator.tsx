1574770a5b274613031a37ca28762004bb28ac35
epolikrati <epolikrati@epignosishq.com>
2024-10-24 01:46:48 +0300

    refactor(select.tsx): add types to custom clear indicator

--- src/components/FormElements/Select/components/CustomClearIndicator.tsx
+++ src/components/FormElements/Select/components/CustomClearIndicator.tsx
@@ -1,9 +1,11 @@
 import React, { memo } from "react";
-import { components, ClearIndicatorProps } from "react-select";
-import { CustomDivProps } from "../types";
+import { components, ClearIndicatorProps, GroupBase } from "react-select";
+import { CustomDivProps, CustomOption } from "../types";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CustomClearIndicator = (props: ClearIndicatorProps<any, true>): JSX.Element => {
+const CustomClearIndicator = (
+  props: ClearIndicatorProps<CustomOption, boolean, GroupBase<CustomOption>>,
+): JSX.Element => {
   return (
     <components.ClearIndicator
       {...props}

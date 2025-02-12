5e14b01ff4299e7ac6a08d8893df9cc7ffff0129
Isidoros Lemonidis <57404631+lem8n@users.noreply.github.com>
2025-02-12 10:31:25 +0200

    chore: prevent immediate toggle func at ToggleSwitch (#500)
    Co-authored-by: Isidoros Lemonidis <ilemonidis@epignosishq.com>

--- src/components/FormElements/ToggleSwitch/ToggleSwitch.stories.tsx
+++ src/components/FormElements/ToggleSwitch/ToggleSwitch.stories.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { StoryFn } from "@storybook/react";
 import { PreviewIconSVG } from "../../../icons";
-import ToggleSwitch, { ToggleProps } from "./ToggleSwitch";
+import Modal from "../../Modal/Modal";
+import Button from "../../Button/Button";
+import ToggleSwitch, { ToggleProps, ToggleSwitchHandlers } from "./ToggleSwitch";
 
 export default {
   title: "components/Form Elements/ToggleSwitch",
@@ -35,4 +37,40 @@ export const WithSubtitle = Template.bind({});
 
 WithSubtitle.args = {
   subtitle: "This is a subtitle for the toggle",
+};
+
+export const WithMiddleStep = () => {
+  const [preventToggle, setPreventToggle] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleSwitchRef = useRef<ToggleSwitchHandlers>(null);
+
+  const handleToggle = (isChecked: boolean): void => {
+    if (!isChecked) {
+      setIsModalOpen(true);
+    } else {
+      setPreventToggle(true);
+    }
+  };
+
+  const handleModalConfirm = (): void => {
+    setPreventToggle(false);
+    setIsModalOpen(false);
+    toggleSwitchRef.current?.toggle();
+  };
+
+  return (
+    <>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <Modal.Footer>
+          <Button onClick={handleModalConfirm}>Confirm</Button>
+        </Modal.Footer>
+      </Modal>
+      <ToggleSwitch
+        ref={toggleSwitchRef}
+        onChange={handleToggle}
+        labelBefore="With Middle Step"
+        preventToggle={preventToggle}
+      />
+    </>
+  );
 };

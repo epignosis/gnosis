d7d8c253f44a8bb236c561021227c81b22bb0d00
Isidoros Lemonidis <57404631+lem8n@users.noreply.github.com>
2025-03-07 16:53:47 +0200

    chore: add cliboard action icons (#515)
    * chore: add cliboard action icons
    * chore: add fill currentColor
    * chore: fix viewbox fill
    * chore: ditch unused rect
    * Add Tab into number input allowed chars
    * chore: add tab into number input allowed chars
    
    ---------
    
    Co-authored-by: Isidoros Lemonidis <ilemonidis@epignosishq.com>

--- src/icons/actions/index.ts
+++ src/icons/actions/index.ts
@@ -1,3 +1,5 @@
 // Action Icons
 export { default as AddCourseIconSVG } from "./add-course-icon.svg";
 export { default as AddContentSVG } from "./add-content.svg";
+export { default as ClipboardCheckSVG } from "./clipboard-check.svg";
+export { default as ClipboardCrossSVG } from "./clipboard-cross.svg";

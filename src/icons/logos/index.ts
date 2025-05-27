c5948b8c4a8df68142c9279ead403c8a9a9d2204
epolikrati <epolikrati@epignosishq.com>
2025-05-27 11:11:34 +0300

    chore(icons): export hubspot logo icon

--- src/icons/logos/index.ts
+++ src/icons/logos/index.ts
@@ -24,3 +24,4 @@ export { default as GoogleTagManagerSVG } from "./google-tag-manager.svg";
 export { default as OpenSesameLogoSVG } from "./open-sesame.svg";
 export { default as LtiSVG } from "./lti.svg";
 export { default as LtiToolSVG } from "./lti-tool.svg";
+export { default as HubSpotSVG } from "./hubspot.svg";

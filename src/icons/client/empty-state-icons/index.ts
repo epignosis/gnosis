ce2a89313bdde8c96f8c665e95d50332facc499d
Konstantinos Blounas <kblounas@epignosishq.com>
2025-06-12 13:02:31 +0300

    chore(icon): analytics empty state icon

--- src/icons/client/empty-state-icons/index.ts
+++ src/icons/client/empty-state-icons/index.ts
@@ -1,3 +1,4 @@
 export { default as ChartSVG } from "./chart.svg";
 export { default as ScatterSVG } from "./scatter.svg";
 export { default as BarsSVG } from "./bars.svg";
+export { default as TilesSVG } from "./tiles.svg";

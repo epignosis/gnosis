c4d7cbe27bfd8ad8e979051389284265c8cc90c1
Isidoros Lemonidis <ilemonidis@epignosishq.com>
2025-06-27 13:47:52 +0300

    fix: fix tests

--- src/components/Drawer/Drawer.test.tsx
+++ src/components/Drawer/Drawer.test.tsx
@@ -39,7 +39,7 @@ describe("<Drawer/>", () => {
     expect(header).toHaveTextContent(headerTxt);
     expect(body).toHaveTextContent(bodyTxt);
     expect(footer).toHaveTextContent(footerTxt);
-    expect(mockFn).toHaveBeenCalledTimes(2);
+    expect(mockFn).toHaveBeenCalledTimes(1);
   });
 
   it("renders correctly without Header and Footer", () => {

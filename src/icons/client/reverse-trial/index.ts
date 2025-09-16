2ffe87bb20def7d99d3e00f60d024c42335a0654
Alexander Antoniades <xanderantoniadis@gmail.com>
2025-09-16 11:27:18 +0300

    chore(reverse trial svg): remove unused svgs from reverse trial

--- src/icons/client/reverse-trial/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ReverseTrialStartSVG } from "./reverse-trial-start.svg";
-export { default as ReverseTrialEndSVG } from "./reverse-trial-end.svg";
-export { default as ReverseTrialEndsInADaySVG } from "./reverse-trial-ends-in-a-day.svg";

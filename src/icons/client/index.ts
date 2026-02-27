0d73fc1e89be8b35782ecf96e4776318b60090c9
Isidoros Lemonidis <57404631+lem8n@users.noreply.github.com>
2026-02-27 11:32:14 +0200

    chore: add two icons (#625)

--- src/icons/client/index.ts
+++ src/icons/client/index.ts
@@ -256,6 +256,8 @@ export { default as EndingFlagSVG } from "./ending-flag.svg";
 export { default as WaffleSVG } from "./waffle.svg";
 export { default as ClockTrialSVG } from "./clock-trial.svg";
 export { default as CrownSVG } from "./crown.svg";
+export { default as TableAddAboveSVG } from "./table-add-above.svg";
+export { default as TableRemoveBelowSVG } from "./table-remove-below.svg";
 
 // Main Menu Icons
 export { default as TeamsSVG } from "./teams.svg";

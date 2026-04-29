945d84adc868077f0cb55594e42c8154d3f3f675
Marios Tsigkas <mariosh346@gmail.com>
2026-04-29 11:01:38 +0300

    ci(release.yml): remove @semantic-release/git, instead open a PR to sync version and changelog
    - Removed @semantic-release/git from package.json and package-lock.json.

--- release.config.cjs
+++ release.config.cjs
@@ -24,12 +24,5 @@ module.exports = {
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
   ],
 };
